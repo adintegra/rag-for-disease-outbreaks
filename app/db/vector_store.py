e0e46c0a5be91f8a2747acaea72d006071c799e4
Mark Kunzmann <mark.kunzmann@adintegra.com>
2025-02-06 11:27:05 +0100

    feat: Updated data structure

--- app/db/vector_store.py
+++ app/db/vector_store.py
@@ -29,11 +29,16 @@ class Document(Base):
   contents = Column(Text)
   url = Column(Text)
   published_at = Column(DateTime)
+  summary = Column(Text, nullable=True)
+  batch = Column(Integer, nullable=True)
 
-  def __init__(self, url, result_all, result_no_stop_words):
+  def __init__(self, meta, contents, url, published_at, summary, batch):
+    self.meta = meta
+    self.contents = contents
     self.url = url
-    self.result_all = result_all
-    self.result_no_stop_words = result_no_stop_words
+    self.published_at = published_at
+    self.summary = summary
+    self.batch = batch
 
   def __repr__(self):
     return "<id {}>".format(self.id)
@@ -57,10 +62,27 @@ class Embedding(Base):
   embedding_4096 = Column(HALFVEC(4096))
   embedding_8192 = Column(HALFVEC(8192))
 
-  def __init__(self, url, result_all, result_no_stop_words):
-    self.url = url
-    self.result_all = result_all
-    self.result_no_stop_words = result_no_stop_words
-
   def __repr__(self):
     return "<id {}>".format(self.id)
+
+
+# class DocEmbeddingView(Base):
+#   __tablename__ = "v_doc_embedding"
+
+#   document_id = Column(Integer, primary_key=True)
+#   embedding_id = Column(Integer, primary_key=True)
+#   chunk_id = Column(Integer)
+#   model = Column(Text)
+#   meta = Column(JSON)
+#   contents = Column(Text)
+#   url = Column(Text)
+#   published_at = Column(DateTime)
+#   embedding = Column(HALFVEC)
+
+#   __table_args__ = {"extend_existing": True}
+
+#   def __init__(self):
+#     pass
+
+#   def __repr__(self):
+#     return "<id {}>".format(self.id)
